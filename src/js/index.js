6016faf6cb7c4f13a20c05ef2588b15ea30984d4
marciojr444 <marcioconinckjunior@gmail.com>
2025-01-30 08:37:09 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,21 @@
 const btnAvancar = document.getElementById("btn-avancar");
-const btnVoltar = document.getElementById("btn-voltar")
+const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
+
+cartoes.forEach(cartao => {
+  cartao.addEventListener("click", function() {
+    const cartaVirada = cartao.querySelector(".carta-virada");
+
+    // virar o cartão
+    cartao.classList.toggle("virar");
+    // mostrar o fundo da carta
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+  });
+});
 
 btnAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return;
@@ -15,7 +29,7 @@ btnAvancar.addEventListener("click", function () {
 btnVoltar.addEventListener("click", function () {
   if (cartaoAtual === 0) return;
 
-  esconderCartaoSelecionado()
+  esconderCartaoSelecionado();
 
   cartaoAtual--;
   mostrarCartao(cartaoAtual);
